ef3806fce54bc236056de7c51201369cae98a571
RALBERSME <149826808+RALBERSME@users.noreply.github.com>
2024-05-01 11:29:44 +0200

    first commit

---
+++ play.js
@@ -0,0 +1,40 @@
+const elem = document.querySelector(".knob");
+const tableau = document.querySelector(".tableau");
+const frog = document.getElementById("frog");
+elem.onclick = () => {
+  tableau.style.animation = "rollingTableau 6s 1s linear forwards";
+  frog.style.animation = "disappearfrog 1.8s 0s linear forwards";
+};
+
+//drag & drop
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+  localStorage.setItem("choosenImageId", data);
+}
+
+// Welcome Greeting;
+
+const inp = document.getElementById("input");
+const welcome = document.getElementById("welcome");
+const starting = document.getElementById("starting");
+let user = "";
+inp.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    user = inp.value;
+    localStorage.setItem("nickname", user);
+    welcome.textContent = `Welcome on the playground 
+    ${user.toUpperCase()} !!`;
+    starting.style.display = "block";
+  }
+});
